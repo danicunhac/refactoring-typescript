0339c1ee4c19b150f0c5c92fc06f364ed52e8896
Daniel Costa <danielcc.jp@gmail.com>
2021-05-27 22:28:57 -0300

    refactor: header component

--- src/components/Food/index.tsx
+++ src/components/Food/index.tsx
@@ -1,8 +1,7 @@
-import { Component, useState } from 'react';
+import { useState } from 'react';
 import { FiEdit3, FiTrash } from 'react-icons/fi';
-
-import { Container } from './styles';
 import api from '../../services/api';
+import { Container } from './styles';
 
 interface FoodData {
   id: number;
